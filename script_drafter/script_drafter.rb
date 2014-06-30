24296b3185a27c4d05eb98933a5e74b1a9d331da
dizcology <dizcology@hotmail.com>
2014-06-30 13:22:38 -0500

    Revert "Revert "drafter: response""
    This reverts commit 91bfb0ae6b997ef09071cdf2592d3539257b09fc.

--- script_drafter/script_drafter.rb
+++ script_drafter/script_drafter.rb
@@ -13,7 +13,11 @@ $PATH=""
 
 username=ENV['USERNAME']
 
-$PATH="C:/Users/"+username+"/Google Drive/Knowledge Engineering/Lessons - Basic IV/026/drafter/"  #path to the files
+
+$PATH="C:/Users/yliu/SkyDrive/RM-synced/cogitatio/script_drafter/"  #path to the files
+
+$PATH="C:/Users/"+username+"/Google Drive/Knowledge Engineering/Lessons - Basic IV/032/"  #path to the files
+
 
 $PATH="" unless username=="yliu"
 
@@ -79,6 +83,8 @@ $submit_count=Hash.new(0)
 
 $word_count=Hash.new(0)
 
+$chr=nil
+
 #Dir.chdir($PATH)
 
 #ft=DOCX.open($PATH+f_template+ext)
@@ -100,7 +106,7 @@ anote=0:note
 $tutor=1:tutor line
 $weak=2:weak line
 $average=3:average line
-$strnng=4:strong line
+$strong=4:strong line
 submit=5:submit
 nextb=6:next in branch
 $screen=7:screen
@@ -128,6 +134,7 @@ $br="In-script branching begins."
 $endbr="In-script branching ends."
 $others=""  #not sure if it is necessary to leave a note about students not mentioned, probably not necessary.
 $sib=", skip if behind"
+$tutor=""
 
 $item=0
 $preamble=Hash.new("???")
@@ -136,21 +143,23 @@ $grp={""=>["All"],"-"=>["All"],"w"=>["Weak"], "a"=>["Average"], "s"=>["Strong"],
 
 
 $resp=Hash.new
+$resp_count=Hash.new
 f=File.open("responses.csv","r")
 f.readline
 f.each do |line|
-  a=line.strie.split(",")
-  if $resp[a[0]].nil?
-    $resp[a[0]]=Array.new
+  a=line.strip.split(",")
+  name=a[0].strip.downcase
+  if $resp[name].nil?
+    $resp[name]=Array.new
+    $resp_count[name]=Hash.new
   end
   
   a[2].to_i.times.each do 
-    $resp[a[0]] << [a[1],a[3]]
+    $resp[name] << a[1].strip 
   end
+  $resp_count[name][a[1].strip] = [a[3].to_i,0]  #cap, count
   
 end
-
-
 
 #$content[$item]=Array.new
 
