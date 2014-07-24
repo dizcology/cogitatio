408b0cb29672d49c6df46a4b56da84acbdda6edf
dizcology <dizcology@hotmail.com>
2014-07-24 01:05:26 -0500

    pptx template

--- report/report_ole.rb
+++ report/report_ole.rb
@@ -42,7 +42,39 @@ class String
   end
 end
 
-
+class Application < WIN32OLE
+  def initialize(type)
+    @type=type
+    if @type=="docx"
+      call_str='Word.Application'
+      @state=0
+    elsif @type=="pptx"
+      call_str='PowerPoint.Application'
+      @state=1
+    else
+      print "Unknown template type: "+@type
+      exit
+    end
+    super(call_str)
+    
+    self.Visible=true
+    self.activate
+    self.WindowState=@state
+    self.size(width=400,height=300)
+    self.position(left=100,top=100)
+    
+  end
+  
+  def open(path)
+    if @type=="docx"
+      return self.Documents.Open(path)
+    elsif @type=="pptx"
+      return self.Presentations.Open(path)
+    end
+  end
+  
+  attr_accessor :type
+end
 
 class WIN32OLE
 
@@ -52,10 +84,18 @@ class WIN32OLE
   end
 
   def position(left=0,top=0)
-    self.Move({'Left'=>left,'Top'=>top})
+    self.Left=left
+    self.Top=top
   end
 
 
+  def each symb, &block
+    count = self.send(symb).Count
+    (1..count).each do |i|
+      yield self.send(symb).Item({'index'=>i})
+    end
+  end
+  
   def gsub(old,new)  
     self.Selection.HomeKey(unit=6)
     find=self.Selection.Find
@@ -183,6 +223,15 @@ puts tle1
 metric_path=getfilepath(tle1)
 metric_path_R="\""+metric_path.gsub("\\","/")+"\""
 
+exit if metric_path==""
+
+tle2="Open template file."
+puts tle2
+template_path=getfilepath(tle2)
+template_type=template_path.split(".")[-1]
+
+exit if template_path==""
+
 source_R="\"C:/Users/yliu/SkyDrive/RM-synced/cogitatio/report/agg.r\""
 preamble = <<EOF
   source(#{source_R})
@@ -229,40 +278,50 @@ mf.each do |line|
   $measures << newm
 end
 
-=begin
-desc_stats=["mean","sd","min","max"]
-system=["pA","pB","pC"]
-aggregated=["p75A", "p45B", "p30C"]
 
-system.each do |m|
-  aggregated|=m.mesh(desc_stats,"_")
-end
-
-list=Array.new
-
-(system|aggregated).each do |m|
-  list << Measure.new(m)
-  list[-1].get_value
-end
-=end
-
-
-word=WIN32OLE.new('Word.Application')
-word.Visible=true
-word.activate
-word.WindowState=0
-word.size(width=400,height=300)
-word.position(left=100,top=100)
-
-doc=word.Documents.Open($RUNPATH+"template.docx")
-
-#print doc.InlineShapes.count
-
+app=Application.new(template_type)
+temp=app.open(template_path)
 
 $measures.each do |m|
   next if m.type=="system" || m.value.nil? || m.type=="Type"
-  word.gsub(m.tag,m.value.round(1).to_s)
+  
+  #print temp.Slides.send(:Count)
+  temp.each :Slides do |s|
+    s.each :Shapes do |sh|
+      if sh.TextFrame.HasText==-1
+        txt=sh.TextFrame.TextRange.Text.gsub(m.tag,m.value.round(1).to_s)
+        sh.TextFrame.TextRange.Text=txt
+      end
+    end
+  end
 end
+
+temp.each :slides do |s|
+  s.each :Shapes do |sh|
+
+    if sh.HasChart==-1
+      cd=sh.Chart.ChartData
+      cd.activate
+      wrksht=cd.Workbook.Worksheets(1)
+      
+      $measures.each do |m|
+        next if m.type=="system" || m.value.nil? || m.type=="Type"
+        wrksht.xgsub(m.tag,m.value.round(1).to_s)
+      end
+      
+      cd.Workbook.Close
+    end
+  end
+end
+
+exit
+
+$measures.each do |m|
+  next if m.type=="system" || m.value.nil? || m.type=="Type"
+  app.gsub(m.tag,m.value.round(1).to_s)
+end
+
+exit
 
 shapes_count=doc.InlineShapes.count
 
@@ -282,37 +341,3 @@ end
 doc.SaveAs($PATH+"out.docx")
 $measures.dump($PATH+"measures_out.csv")
 
-exit
-###################################################
-###################################################
-
-tag1="Table"
-tag2="CHART"
-pic=word.insert(tag1,img=kitten,scale=50, replace=false)
-
-chrt=word.insertchart(tag2, type=51, replace=true).Chart
-chrt.ChartData.Activate
-wrksht=chrt.ChartData.Workbook.Worksheets(1)
-#puts wrksht.ole_methods
-#gets
-#puts chrt.SeriesCollection.ole_methods
-#puts chrt.SeriesCollection.count
-#puts chrt.SeriesCollection(1).ole_methods
-#gets
-#exit
-
-#puts wrkbk.Worksheets(1).UsedRange.ClearContents
-#print wrkbk.Worksheets(1).Range("B1:B2").value
-wrksht.Range("A2:A4").value=[["A"],["B"],["C"]]
-wrksht.Range("B2:B7").value=[[list[4].value],[list[3].value],[list[4].value],[list[3].value],[list[4].value],[list[3].value]]
-puts chrt.SeriesCollection.count
-chrt.SeriesCollection.NewSeries
-wrksht.Range("E1:E2").value=[["NAME"],[list[3].value]]
-puts chrt.SeriesCollection(1).name="WOOPIE!"
-
-
-exit
-doc.SaveAs($PATH+"out_ole.docx")
-#word.Activate
-#ord.WindowState=1
-exit
